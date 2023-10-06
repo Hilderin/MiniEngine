3700c7d2baf0b91ee36057356cc2e2244126d15a
Hilderin <81109165+Hilderin@users.noreply.github.com>
2023-10-06 16:01:13 -0400

    Add some labs

---
+++ CSharp/MiniEngine.Lab/Lab_StructMutableImmutable.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniEngine.Lab
+{
+    internal class Lab_StructMutableImmuatable
+    {
+        private MutableStruct _m = new MutableStruct(1, 1);
+        private ImmutableStruct _im = new ImmutableStruct(1, 1);
+
+        private MutableStruct M { get { return _m; } }
+
+        public void Test()
+        {
+
+
+            //Normal ref...
+            _m = new MutableStruct(1, 1);
+            PassMutableStructRef(ref _m);
+            Console.WriteLine($"_m after ref: {_m.X}, expected 2");
+
+
+            //Updating values using property and method...
+            _m = new MutableStruct(1, 1);
+            this.M.UpdateValues(2, 2);
+            Console.WriteLine($"_m after using property and method: {_m.X}, expected 2");
+
+
+            //Testing in
+            _m = new MutableStruct(1, 1);
+            PassMutableStructIn(_m);
+            Console.WriteLine($"_m after in: {_m.X}, expected 2");
+        }
+
+        private void PassMutableStructRef(ref MutableStruct s)
+        {
+            s.UpdateValues(2, 0);
+        }
+
+        private void PassMutableStructIn(in MutableStruct s)
+        {
+            s.UpdateValues(2, 0);
+        }
+
+        private struct MutableStruct
+        {
+            public float X;
+            public float Y;
+
+            public MutableStruct(float x, float y)
+            {
+                X = x;
+                Y = x;
+            }
+
+            public void UpdateValues(float x, float y)
+            {
+                X = x;
+                Y = x;
+            }
+        }
+
+        private readonly struct ImmutableStruct
+        {
+            public readonly float X;
+            public readonly float Y;
+
+            public ImmutableStruct(float x, float y)
+            {
+                X = x;
+                Y = x;
+            }
+
+        }
+    }
+}
---
+++ CSharp/MiniEngine.Lab/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+namespace MiniEngine.Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Stopwatch sw = Stopwatch.StartNew();
+
+
+                var lab = new Lab_StructMutableImmuatable();
+
+                lab.Test();
+
+
+                sw.Stop();
+                Console.WriteLine("Done: " + sw.ElapsedMilliseconds.ToString("0") + "ms");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
