e5c31a466a02ab2bef6b1b386dae1a8d851b92ed
Hilderin <81109165+Hilderin@users.noreply.github.com>
2023-10-24 06:36:45 -0400

    Update RenderCommandBuffer.cs

--- CSharp/MiniEngine.Drivers.Vulkan/RenderCommandBuffer.cs
+++ CSharp/MiniEngine.Drivers.Vulkan/RenderCommandBuffer.cs
@@ -9,21 +9,21 @@ namespace MiniEngine.Drivers.Vulkan
     public class RenderCommandBuffer: CommandBuffer
     {
         private RenderPass _renderPass;
-        private int _imageIndex;
+        //private int _imageIndex;
 
         private RenderPassBeginInfo _renderPassBeginInfo;
 
         public RenderCommandBuffer(RenderPass renderPass, int imageIndex)
         {
             _renderPass = renderPass;
-            _imageIndex = imageIndex;
+            //_imageIndex = imageIndex;
 
             _renderPassBeginInfo = new RenderPassBeginInfo
             {
                 Framebuffer = _renderPass.Swapchain.Framebuffers[imageIndex],
                 RenderPass = renderPass,
                 //ClearValues = new ClearValue[] { new ClearValue { Color = new ClearColorValue(new float[] { DateTime.Now.Millisecond % 100f / 100f, 0.87f, 0.75f, 1.0f }) } },
-                ClearValues = new ClearValue[] { new ClearValue { Color = new ClearColorValue(new float[] { 0f, 0.87f, 0.75f, 1.0f }) } },
+                ClearValues = new ClearValue[] { new ClearValue { Color = new ClearColorValue(new float[] { 0f, 0f, 0f, 1.0f }) } },
                 RenderArea = new Rect2D { Extent = renderPass.Device.CurrentExtent }
             };
         }
